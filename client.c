489c3268aada209e80c93174b4d9f51faa273e64
mateusinaciosts <mateusinaciosts@gmail.com>
2023-10-18 22:08:43 -0300

    fix: correcting errors

--- client.c
+++ client.c
@@ -1,82 +1,84 @@
 
 #include "minitalk.h"
 
-int	g_bit = 0;
+char	g_wait;
 
-void	received(int sig)
+void	handler(int signum, siginfo_t *info, void *context)
 {
-	if (sig == SIGUSR1)
-		g_bit++;
-	else if (sig == SIGUSR2)
-	{
-		writer(1, 'Send letter\n', 13);
-		g_bit++;
-	}
+	(void)context;
+	(void)info;
+	if (signum == SIGUSR1)
+		ft_printf("char sended!\n");
 	else
+		g_wait = 0;
+}
+
+void	init(void)
+{
+	struct sigaction	sa;
+
+	ft_bzero(&sa, sizeof (sa));
+	sa.sa_flags = 0;
+	sa.sa_sigaction = handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+}
+
+void	putstr(char *c)
+{
+	while (*c)
 	{
-		write(1, "\tError: Signal is invalid.\n", 28);
-		exit(22);
+		write(1, c, 1);
+		c++;
 	}
 }
 
-void	error(char *msg)
-{
-	ft_printf("%s\n", msg);
-	exit(0);
-}
-
-int	check_digit(char *str)
+void	sendbit(pid_t pid, char c)
 {
 	int	i;
 
 	i = 0;
-	while (str[i])
+	while (i < 8)
 	{
-		if (!ft_isdigit(str[i]))
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-void	send_bit(pid_t pid, char byte)
-{
-	int	shift_bits;
-
-	shift_bits = 0;
-	while (shift_bits < 8)
-	{
-		if ((byte >> shift_bits) & 1)
+		if (!g_wait)
 		{
-			kill(pid, SIGUSR1);
+			g_wait	= 1;
+			if (c & 1)
+				kill (pid, SIGUSR1);
+			else
+				kill (pid, SIGUSR2);
+			c >>= 1;
+			i++;
+			usleep(5000);
+		//	g_wait	= 1;
 		}
-		else
-		{
-			kill(pid, SIGUSR2);
-		}
-		usleep(42000);
-		shift_bits++;
 	}
 }
 
-int	main(int argc, char **argv)
+int	main(int ac, char *av[])
 {
+	char	*str;
 	pid_t	pid;
-	int		i;
+	size_t	i;
 
-	signal(SIGUSR1, received);
-	signal(SIGUSR2, received);
-	if (argc != 3)
-		error("Wrong number of arguments.");
-	if (check_digit(argv[1]) || ft_strlen(argv[1]) > 7)
-		error("Invalid PID.");
-	pid = ft_atoi(argv[1]);
 	i = 0;
-	while (argv[2][i])
+	g_wait = 0;
+	init();
+	if (ac != 3)
 	{
-		send_bit(pid, argv[2][i]);
+		ft_printf("./client <process_id> <msg>!\n");
+		return (-1);
+	}
+	pid = ft_atoi(av[1]);
+	str = av[2];
+	while (str[i] != '\0')
+	{
+		sendbit(pid, str[i]);
 		i++;
 	}
-	send_bit(pid, '\n');
+	sendbit(pid, '\n');
+	exit (0);
+	while (1)
+		pause();
 	return (0);
 }
--- server.c
+++ server.c
@@ -1,46 +1,46 @@
 #include "minitalk.h"
 
-char	g_char = 0;
+static char	g_bit;
 
-void	print_char(int signal, siginfo_t *siginfo, void *context)
+void	signal_handle(int signum, siginfo_t *info, void *context)
 {
-	static int	i = 0;
+	static char	temp;
 
-	(void)context;
-	if (signal == SIGUSR1)
-		g_char += (1 << i);
-	if (i == 7)
+	if (g_bit <= 8)
 	{
-		write(1, &g_char, 1);
-		g_char = 0;
-		i = 0;
-		kill(siginfo->si_pid, SIGUSR2);
-		return ;
+		if (signum == SIGUSR1)
+			temp += 1 << g_bit;
+		else
+			temp += 0 << g_bit;
+		g_bit++;
 	}
-	i++;
-	kill(siginfo->si_pid, SIGUSR1);
+	if (g_bit == 8)
+	{
+		write(1, &temp, 1);
+		g_bit = 0;
+		temp = 0;
+		kill(info->si_pid, SIGUSR1);
+	}
+	kill(info->si_pid, SIGUSR2);
+	(void)context;
+}
+
+void	init(void)
+{
+	struct sigaction	sa;
+
+	ft_bzero(&sa, sizeof(sa));
+	sa.sa_sigaction = signal_handle;
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 }
 
 int	main(void)
 {
-	siginfo_t siginfo;
-	struct sigaction sa;
-	sigset_t sigset;
-
-	sa = (struct sigaction){0};
-	sigemptyset(&sigset);
-	sigaddset(&sigset, SIGUSR1);
-	sigaddset(&sigset, SIGUSR2);
-	sa.sa_mask = sigset;
-	sa.sa_flags = SA_SIGINFO;
-	sa.sa_sigaction = print_char;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-	pid_t process_id = getpid();
-	ft_printf("PID: %i\n", process_id);
+	init();
+	ft_printf("server process = [%d]\n", getpid());
 	while (1)
-	{
 		pause();
-	}	
 	return (0);
 }
